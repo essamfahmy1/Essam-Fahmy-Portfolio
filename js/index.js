5509aa0a9763999d35250a4b597fcc9f275e3ef2
Essam Fahmy <essamfahmy1@outlook.com>
2024-01-22 14:21:48 -0500

    adding typing animations and transitions

--- js/index.js
+++ js/index.js
@@ -1,27 +1,28 @@
 $(() => {
 
 
-    const controlTrack = () => {
+    $( "a" ).hide();
 
+    const controlTrack = () => {
         var check = true;
         var count = 0;
 
         const recTrack = () => {
 
-            if(count == 500)
+            if(count == 99999)
             {
                 return;
             }
             else if (check == true)
             {
-                $("#sub").html($("#sub").html().replace('|', ''));
+                $("#sub2").html($("#sub2").html().replace('|', ''));
                 check = false;
                 count++;
                 setTimeout(recTrack, 700);
             }
             else if (check == false)
             {
-                $("#sub").append("|"); 
+                $("#sub2").append("|"); 
                 check = true;
                 count++;
                 setTimeout(recTrack, 700);
@@ -49,7 +50,7 @@ $(() => {
                 setTimeout(printLetter, 80);
               } else {
                 outputLength = 0;
-                controlTrack();
+                $("#sub").html($("#sub").html().replace('|', ''));
                 return;
               }
             
@@ -58,6 +59,35 @@ $(() => {
         printLetter();
     };
 
+    const printSub2 = () => {
+        
+        $("#sub2").empty();
+
+        var outputLength = 0;
+        var data = "Check out my projects below";
+
+        const printLetter = () => {
+
+            if (outputLength < data.length) {
+                $("#sub2").html($("#sub2").html().replace('|', ''));
+                $("#sub2").append(data.charAt(outputLength));
+                $("#sub2").append("|"); 
+
+        
+                outputLength++;
+                setTimeout(printLetter, 80);
+              } else {
+                outputLength = 0;
+                $( "a" ).show(500);
+                controlTrack();
+                return;
+              }
+            
+        };
+
+        printLetter();
+    };
     printSub();
+    setTimeout(printSub2, 2000);
 
 }); // jQuery
