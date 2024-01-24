5c4a825061db568b901c144a419d2561900d9b33
Essam Fahmy <essamfahmy1@outlook.com>
2024-01-24 01:55:52 -0500

    Improved typing animation logic

--- js/index.js
+++ js/index.js
@@ -1,93 +1,33 @@
 $(() => {
+    $(".gitlink").hide();
 
+    $(document).ready(function () {
+        var subtitles = ["Hello! I'm Essam.", "Check out my projects below"];
+        var typingSpeed = 80;
 
-    $( ".gitlink" ).hide();
+        function typeSubtitle(subtitle, targetDiv, callback) {
 
-    const controlTrack = () => {
-        var check = true;
-        var count = 0;
+            var i = 0;
+            $(targetDiv).addClass('typing-subtitle');
 
-        const recTrack = () => {
-
-            if(count == 99999)
-            {
-                return;
+            function typeChar() {
+                if (i < subtitle.length) {
+                    $(targetDiv).append(subtitle.charAt(i));
+                    i++;
+                    setTimeout(typeChar, typingSpeed);
+                }
+                else if (callback) {
+                    $(targetDiv).removeClass('typing-subtitle');
+                    callback();
+                }
+                else {
+                    $(targetDiv).addClass('typing-subtitle');
+                    $(".gitlink").show(500);
+                }
             }
-            else if (check == true)
-            {
-                $("#sub2").html($("#sub2").html().replace('|', ''));
-                check = false;
-                count++;
-                setTimeout(recTrack, 700);
-            }
-            else if (check == false)
-            {
-                $("#sub2").append("|"); 
-                check = true;
-                count++;
-                setTimeout(recTrack, 700);
+            typeChar();
+        }
 
-            }
-        };
-        recTrack();
-    };
-    const printSub = () => {
-        
-        $("#sub").empty();
-
-        var outputLength = 0;
-        var data = "Hello! I'm Essam.";
-
-        const printLetter = () => {
-
-            if (outputLength < data.length) {
-                $("#sub").html($("#sub").html().replace('|', ''));
-                $("#sub").append(data.charAt(outputLength));
-                $("#sub").append("|"); 
-
-        
-                outputLength++;
-                setTimeout(printLetter, 80);
-              } else {
-                outputLength = 0;
-                $("#sub").html($("#sub").html().replace('|', ''));
-                return;
-              }
-            
-        };
-
-        printLetter();
-    };
-
-    const printSub2 = () => {
-        
-        $("#sub2").empty();
-
-        var outputLength = 0;
-        var data = "Check out my projects below";
-
-        const printLetter = () => {
-
-            if (outputLength < data.length) {
-                $("#sub2").html($("#sub2").html().replace('|', ''));
-                $("#sub2").append(data.charAt(outputLength));
-                $("#sub2").append("|"); 
-
-        
-                outputLength++;
-                setTimeout(printLetter, 80);
-              } else {
-                outputLength = 0;
-                $( ".gitlink" ).show(500);
-                controlTrack();
-                return;
-              }
-            
-        };
-
-        printLetter();
-    };
-    printSub();
-    setTimeout(printSub2, 2000);
-
+        typeSubtitle(subtitles[0], '#sub', function () { typeSubtitle(subtitles[1], '#sub2'); });
+    });
 }); // jQuery
